e334de9aa490f0ffd405ae2ce010c0f381b174ac
Daniel Kazakov <danielkgb@email.com>
2024-03-22 10:41:31 +0300

    added registration page

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login-page')
+    path('login/', views.login_view, name='login-page'),
+    path('registration/', views.registration_view, name='registration-page')
 ]
--- users/views.py
+++ users/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def login_view(request):
     return render(request, 'users/login.html')
+
+def registration_view(request):
+    return render(request, 'users/registration.html')
